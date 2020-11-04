a33504e7e9f8ebe262328a12f1f76250b0463204
robasonite <robert@robasonite.com>
2020-11-04 09:01:27 -0500

    Updated Desk Buddy to use xbacklight for hardware screen brightness.

--- scripts/Python/desk-buddy/README.md
+++ scripts/Python/desk-buddy/README.md
@@ -5,10 +5,41 @@ This python program helps take the hassel out of getting volume and brightness k
 
 ## What it can do right now
 
-- Dim or brighten the screen with `xrandr` commands
+- Dim or brighten the screen with `xrandr` or `xbacklight` commands
 - Raise, lower, and mute master volume with `amixer`
+
+
+## Screen brightness adjustment
+
+### Software only
+
+The method with `xrandr` is the *safe* method. The brightness of the screen is modified with *software*. The physical hardware backlight is not affected. This is the recommended option for users who don't want to modify system files. It's also the default option.
+
+
+### Hardware, easy mode
+
+If you want "true" backlight adjustment, first check if the program `xbacklight` will work on your system. Depending on your chosen distribution, you may need to modify the permissions of files under `/sys/class/backlight/<manufacturer>` and/or put the following in your `/etc/X11/xorg.conf` file:
+
+``` shell
+Section "Device"
+    Identifier  "Intel Graphics" 
+    Driver      "intel"
+    Option      "Backlight"  "intel_backlight"
+EndSection<Paste>
+```
+
+**Please keep in mind that this program was only tested on a laptop with Intel graphics. If your laptop has non-Intel graphics (Nvida, Radeon, etc), the above snippet will need to be modified.**
+
+
+Once you get `xbacklight` to work, make sure you set "hwbr" to "true". `.deskbuddyrc`
+
+
+### Hardware, hard mode
+
+This requires direct modification of the program to correctly read and manipulate the files under `/sys/class/backlight`. If you want to go this route, you're on your own.
+
 
 
 ## Tested on
 
-- Debian 10.6 x86\_64
+- Debian 10.6 x86\_64 with Intel graphics
--- scripts/Python/desk-buddy/deskbuddy.py
+++ scripts/Python/desk-buddy/deskbuddy.py
@@ -2,12 +2,27 @@
 # 
 # Desk Buddy by Robert Kight, Copyright 2020.
 #
-# This is a little helper program for minimal environments based around window managers like dwm, spectrwm, i3wm, etc. Such setups usually don't rely on background programs to manage things like screen brightness and audio volume levels. The point of this program is to keep users from cluttering up their configuration files with long and hacky commands. Put them here instead and use the convenience functions in your WM config file.
+# This is a little helper program for minimal environments based around
+# window managers like dwm, spectrwm, i3wm, etc. Such setups usually don't
+# rely on background programs to manage things like screen brightness and
+# audio volume levels. The point of this program is to keep users from
+# cluttering up their configuration files with long and hacky commands.
+# Put them here instead and use the convenience functions in your WM config
+# file.
 #
 # Currently implemented features:
 #
-# - Use 'xrandr' to change the fucking brightness.
+# - Use 'xrandr' to change the brightness (software only, does not control
+# screen backlight.
 # - Use 'amixer' to change the volume.
+#
+# NOTE: When trying to set hardware brightness, it looks inside
+# /sys/class/backlight. If that directory does not exist, please consult
+# the documentation of your chosen Linux/BSD distribution to figure out where
+# the brightness and max_brightness files are.
+# File permissions are another problem. Sometimes the files that need to be
+# modified are locked behind root permissions. Again, consult the
+# documentation to figure out the "proper" way to change these files.
 
 import os
 import sys
@@ -18,24 +33,29 @@ from pathlib import Path
 configFile = str(os.path.join(Path.home(), '.deskbuddyrc'))
 
 # Create a settings file with default data.
-def createConfig():
+def createConfig(configFile):
     print("Unable to read config file '.deskbuddyrc'.")
     print("Attempting to create the file...")
 
     # Default settings dict
     settings = {
-        'brightness': 10,
+        'softBrightness': 10,
+        'softBrightnessStep': 1,
+        'hardBrightness': 50,
+        'hardBrightnessStep': 10,
         'volume': 50,
-        'mute': False
+        'volumeStep': 5,
+        'mute': False,
+        'hardBrightnessEnabled': False
     }
-    writeConfig(settings)
+    writeConfig(settings, configFile)
 
     print("Success!")
     sys.exit()
 
 
 # Read and parse the settings file.
-def readConfig():
+def readConfig(configFile):
 
     # Try to read the file.
     try:
@@ -50,77 +70,129 @@ def readConfig():
     # except (FileNotFoundError, ValueError) as e:
     #except FileNotFoundError:
     except:
-        createConfig()
+        createConfig(configFile)
+        return False
 
 
 # Write the settings to a file.
-def writeConfig(settings):
+def writeConfig(settings, configFile):
     with open(configFile, 'w') as f:
 
         # Convert the setting dictionary to a JSON.
-        jsonSettings = json.dumps(settings)
+        jsonSettings = json.dumps(settings, sort_keys=True, indent=4)
         f.write(jsonSettings)
 
 
 # Run the necessary command to set brightness.
 # Lowers brightness by default.
-def setBrightness(brightUp=False, amount=1):
+# Defaults the 'xrandr' for setting SOFTWARE brightness.
 
-    # Get current settings.
-    currentSettings = readConfig()
+# Set HARDWARE screen brightness with xbacklight.
+def setBrightnessXbacklight(brightUp, currentSettings):
+
+    # Get the brightness step value.
+    # Defaults to 10.
+
+    stepValue = 10
+    if 'hardBrightnessStep' in currentSettings.keys():
+        stepValue = currentSettings['hardBrightnessStep']
 
     # Extract the current brightness level.
-    brightness = currentSettings['brightness']
+    brightness = currentSettings['hardBrightness']
 
     # Modify brightness.
     if brightUp:
-        brightness += amount
+        brightness += stepValue
     else:
-        brightness -= amount
+        brightness -= stepValue
         
-    # Update the file with the new value.
-    currentSettings['brightness'] = brightness
-    writeConfig(currentSettings)
-
-    # 'xrandr' expects a floating point value between 0.0 and 1.0.
-    # Let's make sure that we don't pass an invalid value
-    if brightness > 0 and brightness < 11:
+    # 'xbacklight' expects an integer between 10 and 100.
+    # Let's make sure that we don't pass an invalid value.
+    if brightness > 0 and brightness < 101:
 
 
-        cmd = 'xrandr --output eDP-1 --brightness {}'.format(brightness * 0.1)
+        cmd = 'xbacklight -set {}%'.format(brightness)
         os.system(cmd)
+    
+        # Update the file with the new value.
+        currentSettings['hardBrightness'] = brightness
+
+        
+    return currentSettings
+
+
+# Set SOFTWARE screen brightness with xrandr.
+def setBrightnessXrandr(brightUp, currentSettings):
+   
+    # First check if 'hwbrt
+    if currentSettings['hardBrightnessEnabled']:
+        currentSettings = setBrightnessXbacklight(brightUp, currentSettings)
+    else:
+
+        # Get the brightness step value.
+        # Defaults to 1.
+
+        stepValue = 1
+        if 'softBrightnessStep' in currentSettings.keys():
+            stepValue = currentSettings['softBrightnessStep']
+
+        # Extract the current brightness level.
+        brightness = currentSettings['softBrightness']
+
+        # Modify brightness.
+        if brightUp:
+            brightness += stepValue
+        else:
+            brightness -= stepValue
+            
+        # 'xrandr' expects a floating point value between 0.0 and 1.0.
+        # Let's make sure that we don't pass an invalid value.
+        if brightness > 0 and brightness < 11:
+
+            # Update the file with the new value.
+            currentSettings['softBrightness'] = brightness
+
+            cmd = 'xrandr --output eDP1 --brightness {}'.format(brightness * 0.1)
+            os.system(cmd)
+
+    return currentSettings
 
 
 # Use amixer to set the volume.
 # Volume is set as a percentage, so an integer between 1 and 100.
-def setVolume(volUp=False, amount=5):
+def setVolume(volUp, currentSettings):
     
-    # Get current settings.
-    currentSettings = readConfig()
-
     # Extract the current volume level.
     vol = currentSettings['volume']
 
-    if volUp:
-        vol += amount
-    else:
-        vol -= amount
-    
-    # Update the file
-    currentSettings['volume'] = vol
-    writeConfig(currentSettings)
+    # Get the volume step.
+    # Defaults = 5.
 
+    stepValue = 5
+    if 'volumeStep' in currentSettings.keys():
+        stepValue = currentSettings['volumeStep']
+        
+
+    if volUp:
+        vol += stepValue
+    else:
+        vol -= stepValue
+    
     # Validate the volume for lowering or raising.
     if vol > -1 and vol < 101:
 
         cmd = "amixer set Master {}%".format(vol)
         os.system(cmd)
+        
+        # Update the file.
+        currentSettings['volume'] = vol
 
 
-def setMute():
-    # Get current settings.
-    currentSettings = readConfig()
+    return currentSettings
+    
 
+def setMute(currentSettings):
+    
     # Extract the current volume level.
     mute = currentSettings['mute']
 
@@ -135,14 +207,13 @@ def setMute():
         mute = True
         
 
-    # Update the file
+    # Update the file.
     currentSettings['mute'] = mute
-    writeConfig(currentSettings)
+
+    return currentSettings
 
 
 def main():
-    # print(configFile)
-    # print(readCurrentBrightness())
     help_message = """
 Desk Buddy v0.0.1, Copyright 2020 Robert Kight
 License: MIT
@@ -155,23 +226,32 @@ volume_down - Decrease volume
 mute        - Toggle audio mute
     """
 
-    # Make sure that we have a valid command to work with.
-    if len(sys.argv) > 1:
-        #print(sys.argv[1])
-        if sys.argv[1] == 'bright_up':
-            setBrightness(True)
-        elif sys.argv[1] == 'bright_down':
-            setBrightness()
-        elif sys.argv[1] == 'volume_up':
-            setVolume(True)
-        elif sys.argv[1] == 'volume_down':
-            setVolume()
-        elif sys.argv[1] == 'mute':
-            setMute()
+    # Read the current config and pass it to each of the command function.
+    # The 'if' statement is here to make sure that nothing runs if the
+    # config file doesn't exist.
+    currentSettings = readConfig(configFile)
+    if currentSettings:
+
+        # Make sure that we have a valid command to work with.
+        if len(sys.argv) > 1:
+            #print(sys.argv[1])
+            if sys.argv[1] == 'bright_up':
+                currentSettings = setBrightnessXrandr(True, currentSettings)
+            elif sys.argv[1] == 'bright_down':
+                currentSettings = setBrightnessXrandr(False, currentSettings)
+            elif sys.argv[1] == 'volume_up':
+                currentSettings = setVolume(True, currentSettings)
+            elif sys.argv[1] == 'volume_down':
+                currentSettings = setVolume(False, currentSettings)
+            elif sys.argv[1] == 'mute':
+                currentSettings = setMute(currentSettings)
+            else:
+                print(help_message)
         else:
             print(help_message)
-    else:
-        print(help_message)
+
+        # Update the settings.
+        writeConfig(currentSettings, configFile)
 
 
 if __name__ == "__main__":
